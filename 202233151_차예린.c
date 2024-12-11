e029f0fb86289047f60b093dfa867dc1278f2586
yerin-lab <181642401+yerin-lab@users.noreply.github.com>
2024-12-12 01:15:46 +0900

    Create 202233151_차예린.c

---
+++ 202233151_차예린.c
@@ -0,0 +1,15 @@
+// Hugging Face 이용해서 text-to-image 변환
+
+from huggingface_hub import InferenceClient
+from PIL import Image
+
+client = InferenceClient("black-forest-labs/FLUX.1-dev", token="hf_xxxxxxxxxxxxxx")
+
+# output is a PIL.Image object
+image = client.text_to_image("Astronaut riding a horse")
+
+# 1. 이미지를 저장
+image.save("output_image.png")  # output_image.png로 저장
+
+# 2. 이미지를 띄우기
+image.show()  # 기본 이미지 뷰어에서 열기
